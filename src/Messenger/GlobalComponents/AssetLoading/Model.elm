64c6231b143b75f0b2651653acc8cac2bbd62865
linsyking <kxiang@umich.edu>
2025-01-11 19:59:21 -0500

    fix: asset loading

--- src/Messenger/GlobalComponents/AssetLoading/Model.elm
+++ src/Messenger/GlobalComponents/AssetLoading/Model.elm
@@ -1,4 +1,4 @@
-module Messenger.GlobalComponents.InitScene.Model exposing (InitOption, genGC)
+module Messenger.GlobalComponents.AssetLoading.Model exposing (InitOption, genGC)
 
 {-| Global component configuration module
 
@@ -10,7 +10,6 @@ A Global Component to show initial loading screen.
 
 import Color
 import Json.Encode as E
-import Messenger.Base exposing (UserEvent(..), loadedResourceNum)
 import Messenger.Component.GlobalComponent exposing (genGlobalComponent)
 import Messenger.Scene.Scene exposing (ConcreteGlobalComponent, GCTarget, GlobalComponentInit, GlobalComponentStorage, GlobalComponentUpdate, GlobalComponentUpdateRec, GlobalComponentView)
 import REGL
@@ -28,7 +27,7 @@ type alias Data =
 
 
 init : InitOption -> GlobalComponentInit userdata scenemsg Data
-init opt _ _ =
+init _ _ _ =
     ( {}
     , { dead = False
       , postProcessor = identity
@@ -40,7 +39,7 @@ update : GlobalComponentUpdate userdata scenemsg Data
 update env _ data bdata =
     let
         dead =
-            loadedResourceNum env.globalData == env.globalData.internalData.totResNum
+            env.globalData.internalData.loadedResNum == env.globalData.internalData.totResNum
     in
     ( ( data, { bdata | dead = dead } ), [], ( env, False ) )
 
@@ -51,7 +50,7 @@ updaterec env _ data bdata =
 
 
 view : GlobalComponentView userdata scenemsg Data
-view env data _ =
+view env _ _ =
     REGL.group []
         (P.clear Color.black
             :: List.map
@@ -61,7 +60,7 @@ view env data _ =
                             15 * cos ((pi / 4) * toFloat i)
 
                         y =
-                             15 * sin ((pi / 4) * toFloat i)
+                            15 * sin ((pi / 4) * toFloat i)
                     in
                     P.circle ( 30 + x, env.globalData.internalData.virtualHeight - 30 + y ) (2 + sin (env.globalData.globalStartTime * 0.005 + 2 * pi * toFloat i / 8)) Color.white
                 )
@@ -75,7 +74,7 @@ gcCon opt =
     , update = update
     , updaterec = updaterec
     , view = view
-    , id = "initscene"
+    , id = "assetloading"
     }
 
 
